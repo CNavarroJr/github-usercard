948d494697fc1afc4d5aeea0848a3403ab7fa574
unknown <navarro89cnp@gmail.com>
2020-01-26 13:01:12 -0600

    This is the finished project

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,6 +3,10 @@
            https://api.github.com/users/<your name>
 */
 
+// This is how to create the axios to get access to the API
+
+
+
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -25,6 +29,7 @@
 */
 
 const followersArray = [];
+
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -53,3 +58,79 @@ const followersArray = [];
   luishrd
   bigknell
 */
+
+
+function userCard(follower){
+  //console.log(follower);
+    let newCard = document.createElement('div');
+  newCard.classList.add('.card');
+  
+  let newImage = document.createElement('img');
+  newImage.classList.add('.image');
+  newImage.src = follower.avatar_url;
+  
+  let info = document.createElement('div');
+  
+  
+  let header = document.createElement('h3');
+  header.textContent = follower.name
+  header.classList.add('.header')
+  
+  let username = document.createElement('p');
+  username.classList.add('.username')
+  username.textContent = `Login: ${follower.login}`
+  
+  let location = document.createElement('p');
+  location.textContent = `Location: ${follower.location}`
+  
+  let profile = document.createElement('p');
+  profile.textContent = `Profile: ${follower.html_url}`
+  
+  let followers = document.createElement('p');
+  followers.textContent = `Followers: ${follower.followers}`
+  
+  let following = document.createElement('p');
+  following.textContent = `Following: ${follower.following}`;
+
+  let bio = document.createElement('p');
+  bio.textContent = `Bio: ${follower.bio}`;
+  
+  newCard.appendChild(newImage);
+  newCard.appendChild(info);
+  info.appendChild(header);
+  info.appendChild(username);
+  info.appendChild(location);
+  info.appendChild(profile);
+  info.appendChild(followers);
+  info.appendChild(following);
+  info.appendChild(bio);
+  
+  newCard.addEventListener('click', () => {
+    newCard.classList.toggle('selected')
+  })
+  return newCard
+  }
+  
+  
+  axios.get('https://api.github.com/users/CNavarroJr')
+    .then((response) => {
+      // console.log(response);
+     
+     var card = document.querySelector(".cards")
+     card.appendChild(userCard(response.data))
+      })
+  
+  axios.get('https://api.github.com/users/CNavarroJr/followers')
+      .then(function (response) {
+        const followersArray = response.data
+  
+        followersArray.forEach((follower) => {
+          axios.get(follower.url)
+          .then(function (response) {
+            var card = document.querySelector(".cards");
+            card.appendChild(userCard(response.data));
+          })
+        })
+      })
+
+
